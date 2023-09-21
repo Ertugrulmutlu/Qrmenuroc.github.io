04d25af7e4f3e7a80cf6b680e73271ba6cfd5aa9
Ertugrulmutlu <65208049+Ertugrulmutlu@users.noreply.github.com>
2023-09-21 23:16:39 +0300

    new site

--- main.js
+++ main.js
@@ -1,129 +1,134 @@
-    // Get references to the button and the section to scroll to
-    const toggleButton = document.getElementById('Kahvaltı_but');
-    const sectionToScroll = document.getElementById('Kahvaltı_sec');
-    const toggleButton_soğuk = document.getElementById('soğuk_but');
-    const sectionToScroll_soğuk = document.getElementById('soğuk_sec');
-    const toggleButton_sıcak = document.getElementById('sıcak_but');
-    const sectionToScroll_sıcak = document.getElementById('sıcak_sec');
-    const go_button = document.getElementById('go');
-
-    // Add a click event listener to the button
-    toggleButton.addEventListener('click', () => {
-        // Toggle the display property of the section
-        
-
-        if (sectionToScroll.style.display === 'none' || sectionToScroll.style.display === '') {
-            sectionToScroll_soğuk.style.display ='none';
-            sectionToScroll_sıcak.style.display ='none';
-            go_button.style.display ='block';
-
-            sectionToScroll.style.display = 'block';
-        } else {
-            sectionToScroll.style.display = 'none';
-        }
-
-        // Scroll to the section
-        sectionToScroll.scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // Get references to the button and the section to scroll to
-    // Add a click event listener to the button
-    toggleButton_soğuk.addEventListener('click', () => {
-        sectionToScroll.style.display == 'none';
-
-        // Toggle the display property of the section
-        if (sectionToScroll_soğuk.style.display === 'none' || sectionToScroll_soğuk.style.display === '') {
-            sectionToScroll.style.display ='none';
-            sectionToScroll_sıcak.style.display ='none';
-            go_button.style.display ='block';
-
-            sectionToScroll_soğuk.style.display = 'block';
-        } else {
-            sectionToScroll_soğuk.style.display = 'none';
-        }
-
-        // Scroll to the section
-        sectionToScroll_soğuk.scrollIntoView({ behavior: 'smooth' });
-    });
-
-        // Get references to the button and the section to scroll to
-    // Add a click event listener to the button
-    toggleButton_sıcak.addEventListener('click', () => {
-        sectionToScroll.style.display == 'none';
-        sectionToScroll_soğuk.style.display ='none';
-        go_button.style.display ='block';
-
-        // Toggle the display property of the section
-        if (sectionToScroll_sıcak.style.display === 'none' || sectionToScroll_sıcak.style.display === '') {
-            sectionToScroll.style.display ='none';
-
-            sectionToScroll_sıcak.style.display = 'block';
-        } else {
-            sectionToScroll_sıcak.style.display = 'none';
-        }
-
-        // Scroll to the section
-        sectionToScroll_sıcak.scrollIntoView({ behavior: 'smooth' });
-    });
-
-
-    const searchInput = document.getElementById('searchInput');
-    const cards = document.querySelectorAll('#kahvaltı_card'); // Use querySelectorAll to select all cards
-    
-    searchInput.addEventListener('input', () => {
-        const searchTerm = searchInput.value.toLowerCase();
-    
-        cards.forEach((card) => {
-            const cardTitle = card.querySelector('.card-title').textContent.toLowerCase(); // Use querySelector to select card title
-            if (cardTitle.includes(searchTerm)) {
-                card.style.display = 'block'; // Show matching card
-            } else {
-                card.style.display = 'none'; // Hide non-matching card
-            }
-        });
-    });
-
-const searchInput_soguk = document.getElementById('searchInput_soguk');
-const cards_soguk = document.querySelectorAll('#soguk-card');
-
-searchInput_soguk.addEventListener('input', () => {
-const searchTerm = searchInput_soguk.value.toLowerCase();
-
-cards_soguk.forEach((card) => {
-    const cardTitle = card.querySelector('.card-title').textContent.toLowerCase();
-    if (cardTitle.includes(searchTerm)) {
-        card.style.display = 'block'; // Show matching card
-    } else {
-        card.style.display = 'none'; // Hide non-matching card
-    }
-});
+anime({
+  targets: "div.arrow",
+  translateY: [
+    { value: 100, duration: 500 },
+    { value: 0, duration: 800 },
+  ],
+  rotate: {
+    value: "6turn",
+    easing: "easeInOutSine",
+  },
+  delay: function (el, i, l) {
+    return 3500;
+  },
+  loop: true,
 });
 
-const searchInput_sıcak = document.getElementById('searchInput_sıcak');
-const cards_sıcak = document.querySelectorAll('#sıcak-card');
-
-searchInput_sıcak.addEventListener('input', () => {
-const searchTerm = searchInput_sıcak.value.toLowerCase();
-
-cards_sıcak.forEach((card) => {
-    const cardTitle = card.querySelector('.card-title').textContent.toLowerCase();
-    if (cardTitle.includes(searchTerm)) {
-        card.style.display = 'block'; // Show matching card
-    } else {
-        card.style.display = 'none'; // Hide non-matching card
-    }
+anime({
+  targets: "span.letter1",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(5, .1)',
+  loop: false
 });
+anime({
+  targets: "span.letter2",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(5, .2)',
+  loop: false
 });
-
-function go_header(){
-    const sectionToScroll = document.getElementById('Kahvaltı_sec');
-    const sectionToScroll_soğuk = document.getElementById('soğuk_sec');
-    const sectiontoscroll_header = document.getElementById('Header');
-    const sectionToScroll_sıcak = document.getElementById('sıcak_sec');
-    sectionToScroll_sıcak.style.display ='none';
-    sectionToScroll_soğuk.style.display ='none';
-    sectionToScroll.style.display = 'none';
-    go_button.style.display ='none';
-
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+anime({
+  targets: "span.letter3",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(5, .3)',
+  loop: false
+});
+anime({
+  targets: "span.letter4",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(5, .4)',
+  loop: false
+});
+anime({
+  targets: "span.letter5",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(1, .5)',
+  loop: false
+});
+anime({
+  targets: "span.letter6",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(1, .6)',
+  loop: false
+});
+anime({
+  targets: "span.letter7",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(1, .7)',
+  loop: false
+});
+anime({
+  targets: "span.letter8",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(1, .8)',
+  loop: false
+});
+anime({
+  targets: "span.letter9",
+  keyframes: [
+    {translateY: -10},
+    {translateY: 0},
+  ],
+  duration: 3500,
+  easing: 'easeOutElastic(1, .9)',
+  loop: false
+});
+const scroll_animation = anime({
+  targets: '.img-figure',
+  translateX: -1000,
+  loop: false,
+  duration: 2000,
+  easing: 'easeInOutExpo'
+});
+const scroll_animation_2 = anime({
+  targets: '.img-figure-r',
+  translateX: 1000,
+  loop: false,
+  duration: 2000,
+  easing: 'easeInOutExpo'
+});
+const animateOnscroll = function(div,speed= 200,offset=185){
+  const scroll_p = window.pageYOffset - div.offsetTop;
+  return (scroll_p + offset) / speed;
 }
+const section2 = document.querySelector("#slider-food")
+
+window.onscroll = function (){
+  scroll_animation.seek(animateOnscroll(section2)*scroll_animation.duration);
+  scroll_animation_2.seek(animateOnscroll(section2)*scroll_animation_2.duration);
+
+
+}
+
+
